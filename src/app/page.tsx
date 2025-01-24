ca295e1b9b3ddf2d6348f9dac51c098ef8b500ca
bje <bje@immeo.dk>
2025-01-24 15:49:14 +0100

    print all

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 export const dynamic = 'force-dynamic'
 export default function Home() {
   const env =  process.env;
-  return (
-    <h1>{env.PATH}</h1>
+  return (    
+  <div>
+    <h1>Environment Variables:</h1>
+    <ul>
+      {Object.entries(env).map(([key, value]) => (
+        <li key={key}>
+          <strong>{key}:</strong> {value?.toString()}
+        </li>
+      ))}
+    </ul>
+  </div>
   );
 }

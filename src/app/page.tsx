bff1bd99bf24a6600ccc7a1adcc4be4d5975aa84
bje <bje@immeo.dk>
2025-01-24 18:37:14 +0100

    Revert env var write out. No CS was available

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export const dynamic = 'force-dynamic'
 export default function Home() {
-  const guid = uuidv4();
-  const env =  process.env;
   return (    
-  <div>
-    <h1>Environment Variables:</h1>
-    <ul>
-      {Object.entries(env).map(([key, value]) => (
-        <li key={key}>
-          <strong>{key}:</strong> {value?.toString()}
-        </li>
-      ))}
-    </ul>
-    <p>Generated GUID: {guid}</p>
-  </div>
+    <h1>Test</h1>
   );
 }
